630ef52bbbb892646510ed1d4dbfbb4d0d13d423
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-11-26 11:28:59 -0300

    criado RichtextBlock

--- streams/blocks.py
+++ streams/blocks.py
@@ -9,7 +9,16 @@ class TituloETextoBlock(blocks.StructBlock):
     titulo = blocks.CharBlock(required=True)
     texto = blocks.TextBlock(required=True, help_text="Texto sem formatação")
 
-    class Meta:  # noqa
+    class Meta:
         template = "streams/titulo_e_texto_block.html"
         icon = "edit"
         label = "Título & Texto"
+
+
+class RichtextBlock(blocks.RichTextBlock):
+    """Richtext with all the features."""
+
+    class Meta:
+        template = "streams/richtext_block.html"
+        icon = "doc-full"
+        label = "RichText completo"
