b27f10f90db84b1cdae675c676121b3e09483a18
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-11-30 18:49:13 -0300

    só pt e en disponíveis

--- co2/settings/base.py
+++ co2/settings/base.py
@@ -161,3 +161,6 @@ WAGTAIL_SITE_NAME = "co2"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+LANGUAGES = WAGTAILADMIN_PERMITTED_LANGUAGES = [('en', 'English'),
+                                                ('pt', 'Portuguese')]
